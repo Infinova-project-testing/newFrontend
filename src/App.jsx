a2bbc109e3d6e02c9b5205edb84c08cef50e320c
Ibrahim <ikapadwanchwala@gmail.com>
2025-09-07 11:35:18 +0530

    Initial development and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-
-
-
-
+import { Route,Routes } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import HomePage from "./pages/HomePage"
+import DivisionsPage from "./pages/DivisionsPage"
+import AboutUs from "./pages/AboutUs"
+import Clients from "./pages/Clients"
+import CeoPage from "./pages/CeoPage"
+import Footer from "../components/Footer"
+import Careers from "./pages/Careers"
+import ContactUs from "./pages/ContactUs"
+import Eduventures from "./pages/Eduventures"
+import Consultants from "./pages/Consultants"
+import Technology from "./pages/Technology"
+import Mission from "./pages/Mission"
+import Vision from "./pages/Vision"
+import Blog from "./pages/Blog"
 function App() {
   
 
   return (
     <>
-      <h1>Hello</h1>
+    <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+      <Route path='/about-us' element={<AboutUs/>}></Route>
+      <Route path='/Careers' element={<Careers/>}></Route>
+      <Route path='/contact-us' element={<ContactUs/>}></Route>
+      <Route path='/eduventures' element={<Eduventures/>}></Route>
+      <Route path='/Consultants' element={<Consultants/>}></Route>
+      <Route path='/technology' element={<Technology/>}></Route>
+      <Route path='/mission' element={<Mission/>}></Route>
+      <Route path='/vision' element={<Vision/>}></Route>
+      <Route path='/blog' element={<Blog/>}></Route>
+    </Routes>
+      <Navbar/>
+   
+      <DivisionsPage/>
+      <AboutUs/>
+      <Clients/>
+      <CeoPage/>
+      <Footer/>
+      
     </>
   )
 }
